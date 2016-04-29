4771e2fdf310014f9c50e0d92873bc544993fe58
Semyon Belokovsky <igrampe@gmail.com>
2016-04-29 22:23:45 +0500

    Initial Commit

---
+++ FastenTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FastenTest
+//
+//  Created by Semyon Belokovsky on 29/04/16.
+//  Copyright © 2016 igrampe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
